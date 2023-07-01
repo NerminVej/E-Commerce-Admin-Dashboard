199e518e369cdd7393ffb4f0aa97b22483ce1fad
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-07-01 08:32:41 +0200

    added clerk authentication to the project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-      <div>
+      <div className="p-4">
         <Button>Click Me</Button>
       </div>
     )

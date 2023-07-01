4ba44fc82f3a5d26177e1c4785acc07fd5579cd9
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-07-01 08:36:56 +0200

    renamed the home function to be the SetupPage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
-    return (
-      <div className="p-4">
-        <Button>Click Me</Button>
-      </div>
-    )
-  }
-  
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default SetupPage;

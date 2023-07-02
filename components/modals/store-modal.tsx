f4327d4f63e256706a221fdbccc992e8156dad27
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-07-02 08:08:32 +0200

    added and implemented the store-modal

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { Modal } from "@/components/ui/modal";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Button } from "@/components/ui/button";
+
+
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories."
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Future Create Store Form
+    </Modal>
+  );
+};

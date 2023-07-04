41b35c8fdf816de1d626ef86c0aaf953a5087e99
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-07-04 08:00:59 +0200

    implemented the logic of the settings-form

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
-const page = () => {
+import prismadb from "@/lib/prismadb";
+
+import { SettingsForm } from "./components/settings-form";
+
+const SettingsPage = async ({ params }: { params: { storeId: string } }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store} />
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default SettingsPage;

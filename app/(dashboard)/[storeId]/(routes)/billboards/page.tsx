d5bb001766f7afd0a543a721254ae91204912137
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-07-05 09:02:25 +0200

    added the categories folder

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -21,6 +21,8 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
     createdAt: format(item.createdAt, "MMMM do, yyyy"),
   }));
 
+  
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
